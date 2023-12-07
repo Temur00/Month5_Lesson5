7803aa5395e774573c7d9b5aeda207036330675a
Temur <shernazarovtemurcom001@gmail.com>
2023-12-07 16:12:38 +0500

    Some things has been added

---
+++ js/home.js
@@ -0,0 +1,57 @@
+let stars = document.getElementsByClassName("star");
+let output = document.getElementById("output");
+function gfg(n) {
+  remove();
+  for (let i = 0; i < n; i++) {
+    if (n == 1) cls = "one";
+    else if (n == 2) cls = "two";
+    else if (n == 3) cls = "three";
+    else if (n == 4) cls = "four";
+    else if (n == 5) cls = "five";
+    stars[i].className = "star " + cls;
+  }
+  output.innerText = "Рейтинг: " + n + "/5";
+}
+function remove() {
+  let i = 0;
+  while (i < 5) {
+    stars[i].className = "star";
+    i++;
+  }
+}
+
+// Like
+const button = document.querySelector(".heart-like-button");
+
+button.addEventListener("click", () => {
+  if (button.classList.contains("liked")) {
+    button.classList.remove("liked");
+  } else {
+    button.classList.add("liked");
+  }
+});
+
+// MODAL
+const btnOpen = document.getElementById("btn-open");
+const btnClose = document.getElementById("btn-close");
+const modal = document.querySelector(".modal");
+
+btnOpen.addEventListener("click", function (e) {
+  modal.classList.add("open");
+});
+
+btnClose.addEventListener("click", function (e) {
+  modal.classList.remove("open");
+});
+
+// KATALOG
+const ktlgOpen = document.getElementById("katalog");
+const ktlgClose = document.getElementById("katalog");
+
+ktlgOpen.addEventListener("click", function (e) {
+  modal.classList.add("open");
+});
+
+ktlgClose.addEventListener("click", function (e) {
+  modal.classList.remove("open");
+});
